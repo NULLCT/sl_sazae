bc5d85fc9e623b85f22d55b17d3c4c455f7eaac0
NULLCT <cockatiel.u10@gmail.com>
2021-06-23 22:21:49 +0900

    add convert codecs function

--- scraper/main.py
+++ scraper/main.py
@@ -2,16 +2,18 @@
 
 import codecs
 import requests
+import os
 from bs4 import BeautifulSoup
 
 LINK = "http://park11.wakwak.com/~hkn/"
+FILE = "res.txt"
+BF = "res.bf.txt"
 
 def scrapeFromSazae(link):
   bf = requests.get(link);
   bf.encoding = "shift_jis"
   soup = BeautifulSoup(bf.text, "html.parser")
-  print(soup.find("pre").string, file=codecs.open("res.txt","a","shift_jis"))
-
+  print(soup.find("pre").string, file=codecs.open(BF,"a","shift_jis"))
 
 def scrape():
   soup = BeautifulSoup(requests.get(LINK+"bunseki0.htm").text, "html.parser")
@@ -22,4 +24,17 @@ def scrape():
   for link in links:
     scrapeFromSazae(LINK+link)
 
+def convertShiftJisToUTF8(_fin,_fout):
+  fin = codecs.open(_fin,"r","shift_jis")
+  fout = codecs.open(_fout,"w","utf-8")
+
+  for i in fin:
+    fout.write(i)
+
+  fin.close()
+  fout.close()
+
+  os.remove(_fin)
+
 scrape()
+convertShiftJisToUTF8(BF,FILE)
